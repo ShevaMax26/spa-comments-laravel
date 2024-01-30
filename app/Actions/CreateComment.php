74068a11c5a41f29ae734566cfe83244b93a1849
ShevaMax26 <shevchuk.maxym@gmail.com>
2024-01-30 19:46:50 +0200

    mod

--- app/Actions/CreateComment.php
+++ app/Actions/CreateComment.php
@@ -9,27 +9,27 @@ class CreateComment
     public function handle(array $data)
     {
         if (!isset($data['parent_id'])) {
-            return Comment::query()->create([
+            $comment = Comment::query()->create([
+                'user_id' => auth('sanctum')->user()->id,
+                'message' => $data['message'],
+            ]);
+        } else {
+            $parentComment = Comment::query()->findOrFail($data['parent_id']);
+
+            $comment = $parentComment->children()->create([
                 'user_id' => auth('sanctum')->user()->id,
                 'message' => $data['message'],
             ]);
         }
-        else {
-            $parentComment  = Comment::query()->findOrFail($data['parent_id']);
 
-            $childrenComment = $parentComment->children()->create([
-                'user_id' => auth('sanctum')->user()->id,
-                'message' => $data['message'],
-            ]);
-
-            if (isset($data['image'])) {
-                $childrenComment->addMedia($data['image'])->toMediaCollection('images');
-            }
-            if (isset($data['text_file'])) {
-                $childrenComment->addMedia($data['text_file'])->toMediaCollection('text_files');
-            }
-
-            return $childrenComment;
+        if (isset($data['image'])) {
+            $comment->addMedia($data['image'])->toMediaCollection('images');
         }
+
+        if (isset($data['text_file'])) {
+            $comment->addMedia($data['text_file'])->toMediaCollection('text_files');
+        }
+
+        return $comment;
     }
 }
